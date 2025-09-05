d5c34c28bc682cdffcf3fc27e293dc44e59de272
Jade <78374850+JKFUNKO@users.noreply.github.com>
2025-09-04 22:53:53 -0700

    The starting of something

---
+++ js.js
@@ -0,0 +1,24 @@
+
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        var logoBtn = document.getElementById("logoBtn");
+        if (logoBtn) logoBtn.style.display = "none";
+    }
+
+    window.onload = function() {
+        var logoBtn = document.getElementById("logoBtn");
+        if (logoBtn) {
+            logoBtn.onclick = openNav;
+            // Always show logo on load since nav bar is closed
+            logoBtn.style.display = "";
+        }
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        var logoBtn = document.getElementById("logoBtn");
+        if (logoBtn) logoBtn.style.display = "";
+    }
